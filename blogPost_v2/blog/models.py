78a7d7582b2ab9ea183678aa3367b0d1c96bbcaa
gatayaMed <gataya.mohammed@gmail.com>
2025-02-07 09:29:57 +0100

    creation of list articles

--- blogPost_v2/blog/models.py
+++ blogPost_v2/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name  
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    tags = models.ManyToManyField(Tag, related_name="articles")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(default="", null=False)
+    image = models.ImageField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.title
+
+    def related_articles(self):
+        return Article.objects.filter(tags__in=self.tags.all()).exclude(id=self.id).distinct()[:3]  # Limit to 3 related articles
+
+    def latest_articles(self):
+        return Article.objects.order_by('-created_at')[:3]  # Latest 3 articles
--- blogPost_v2/blog/views.py
+++ blogPost_v2/blog/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
-
+from .models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def homeView(request):
-    return render ( request, 'home.html', {})
+    return render ( request, 'blog/home.html', {})
+
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    latest_articles = Article.objects.order_by('-created_at')[:3]
+    paginator = Paginator(articles, 3)
+    page_number = request.GET.get('page', 1)
+   
+    
+    try:
+        articles = paginator.page(page_number)
+        nums ="a"*articles.paginator.num_pages
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    return render(request, 'blog/article_list.html', {'articles': articles, 'latest_articles': latest_articles, 'nums':nums})
+
+# def article_list(request):
+#     articles = Article.objects.all()
+#     latest_articles = Article.objects.order_by('-created_at')[:3]
+#     return render(request, 'blog/article_list.html', {'articles': articles, 'latest_articles': latest_articles})
+
+# class ArticleListView(ListView):
+#     """
+#     Alternative post list view
+#     """
+#     model = Article
+#     context_object_name = 'articles',
+#     paginate_by = 3
+#     template_name = 'blog/article_list.html'
