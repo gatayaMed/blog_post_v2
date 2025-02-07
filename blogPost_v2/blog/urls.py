b97923c7e1052d026ff9f44889242098b645d90a
gatayaMed <gataya.mohammed@gmail.com>
2025-02-07 10:15:06 +0100

    I have the urls  for articles_list and articles_details , contact and about in urls.py. changing the base.html from templates folder to blog folder with other templates

--- blogPost_v2/blog/urls.py
+++ blogPost_v2/blog/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
-from . views import homeView
+from . views import homeView,article_list,article_detail,contact,about
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("", homeView, name="home"),
-
-]
+    path('article_list/', article_list, name='article_list'),
+    path('<int:article_id>/', article_detail, name='article_detail'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
